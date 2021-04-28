8b370d97f466c45bc838b302549b04a25bfd8dd7
itgaofee <ccoffeee@163.com>
2021-04-28 16:05:19 +0800

    修改和删除

--- src/main/java/com/gaofei/sysmanager/controller/UserController.java
+++ src/main/java/com/gaofei/sysmanager/controller/UserController.java
@@ -13,6 +13,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
 
+import java.util.Arrays;
 import java.util.Date;
 import java.util.List;
 
@@ -47,7 +48,8 @@ public class UserController {
 
     @RequestMapping("add")
     public CommonResult add( @RequestBody User user){
-        boolean save = userService.save(user);
+        System.out.println(user);
+        boolean save = userService.saveOrUpdate(user);
         return CommonResult.success(save,"保存成功");
     }
 
@@ -57,6 +59,21 @@ public class UserController {
     public CommonResult faceUpload(MultipartFile[] file){
         List<FileResult> upload = fileService.upload(file);
         return CommonResult.success(upload,"upload成功");
+    }
+
+    @RequestMapping("del")
+    public CommonResult del(@RequestParam("ids") Integer[] ids){
+
+        userService.removeByIds(Arrays.asList(ids));
+
+        return CommonResult.success(null, "成功");
+    }
+    @RequestMapping("getOne")
+    public CommonResult getOne(Integer id){
+
+        User user = userService.getById(id);
+
+        return CommonResult.success(user, "成功");
     }
 }
 
